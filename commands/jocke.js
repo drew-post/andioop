8140eccfea46fef27d58f60620302269c3304ebf
Drew Post <44125751+drew-post@users.noreply.github.com>
2019-07-28 05:37:18 -0500

    added link

--- commands/jocke.js
+++ commands/jocke.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'jocke',
     description: 'lubs jocke',
     execute(message) {
-        message.channel.send('i lub jocke :heart:');
+        message.channel.send('i lub jocke :heart:\nhttps://www.youtube.com/watch?v=966I5NCjnsg');
     },
 };
